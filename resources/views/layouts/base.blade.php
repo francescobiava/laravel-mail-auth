d069d8b1b6b832ce1401e4309982a8e18e93c440
Francesco <franc.ns@gmail.com>
2020-01-23 16:44:01 +0100

    added CRUD

---
+++ resources/views/layouts/base.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+  <title>BLOG</title>
+</head>
+<body>
+
+  <header>
+    <a href="{{ route('index') }}">
+      <h1>
+        BLOG
+        @auth
+          {{ Auth::user()->name }}
+        @else
+          (guest)
+        @endauth
+      </h1>
+    </a>
+  </header>
+
+  <div class="content">
+    @yield('content')
+  </div>
+
+  <footer>
+    <p>Bye Bye</p>
+  </footer>
+  
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,19 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes();
+Route::get('/home', 'HomeController@index') -> name('home');
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/', 'MainController@index') -> name('index');
+
+Route::get('/category/edit/{id}', 'MainController@categoryEdit') -> name('category.edit');
+Route::post('/category/edit/{id}', 'MainController@categoryUpdate') -> name('category.update');
+Route::get('/category/delete/{id}', 'MainController@categoryDelete') -> name('category.delete');
+
+Route::get('/posts/show/{id}', 'MainController@postShow') -> name('post.show');
+Route::get('/post/edit/{id}', 'MainController@postEdit') -> name('post.edit');
+Route::post('/post/edit/{id}', 'MainController@postUpdate') -> name('post.update');
+Route::get('/post/delete/{id}', 'MainController@postDelete') -> name('post.delete');
+
+Route::get('/category/post/{id}', 'MainController@categoryPost') -> name('category.post');
+Route::post('/category/post/{id}', 'MainController@categoryPostCreate') -> name('category.post.create');
