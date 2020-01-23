33e2d25a9e54e1a3d2b8aa2394fa2cb4ffc76f98
Francesco <franc.ns@gmail.com>
2020-01-23 18:19:24 +0100

    added authentication to delete a category

--- routes/web.php
+++ routes/web.php
@@ -18,12 +18,12 @@ Route::get('/', 'MainController@index') -> name('index');
 
 Route::get('/category/edit/{id}', 'MainController@categoryEdit') -> name('category.edit');
 Route::post('/category/edit/{id}', 'MainController@categoryUpdate') -> name('category.update');
-Route::get('/category/delete/{id}', 'MainController@categoryDelete') -> name('category.delete');
+Route::get('/category/delete/{id}', 'MainController@categoryDelete') -> name('category.delete') -> middleware('auth');
 
 Route::get('/posts/show/{id}', 'MainController@postShow') -> name('post.show') -> middleware('auth');
 Route::get('/post/edit/{id}', 'MainController@postEdit') -> name('post.edit');
 Route::post('/post/edit/{id}', 'MainController@postUpdate') -> name('post.update');
 Route::get('/post/delete/{id}', 'MainController@postDelete') -> name('post.delete');
 
-Route::get('/category/post/{id}', 'MainController@categoryPost') -> name('category.post');
+Route::get('/category/post/{id}', 'MainController@categoryPost') -> name('category.post') -> middleware('auth');
 Route::post('/category/post/{id}', 'MainController@categoryPostCreate') -> name('category.post.create');
